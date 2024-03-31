3e6f8d60ae88df26a01a7710271d38251e944f67
jonathan.smith.67 <jonathan.smith.67@us.af.mil>
2024-03-31 12:56:31 -0600

    Created a script to print variables information.

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) {
+    String first_name = "John";
+    String last_name = "Smith";
+    String street_address = "1234 Elm Street";
+    String city = "Springfield";
+    String zip_code = "12345";
+
+    System.out.println(first_name);
+    System.out.println(last_name);
+    System.out.println(street_address);
+    System.out.println(city);
+    System.out.println(zip_code);
+
+    }
+}
